5642f8db8e95ed2650800bedb9db56451f661caf
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-04 14:23:24 +0300

    src klasoru icindeki assets klasoru icindeki desktop-background.png dosyasina desktop gorunum icin app.css dosyasinda css islemi uygulandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,46 @@
-import CreditCardBack from './assets/img/mobile-credit-card-back.png'
-import CreditCardFront from './assets/img/mobile-credit-card-front.png'
+import MobileCreditCardBack from './assets/img/mobile-credit-card-back.png'
+import MobileCreditCardFront from './assets/img/mobile-credit-card-front.png'
 import MobileBackground from './assets/img/mobile-background.png'
+import DesktopBackground from './assets/img/desktop-background.png'
 import './App.css'
 
 export default function App() {
 
   return (
     <>
-      <div className="credit-card-visual">
-        <img src={CreditCardBack} className='credit-card-back' alt="" />
-        <img src={CreditCardFront} className='credit-card-front' alt="" />
-        <img src={MobileBackground} className='mobile-background' alt="" />
-      </div>
-      <div className="credit-card-informations">
-        <div className="credit-card-name">
-          <label>KART SAHİBİNİN ADI</label>
-          <input type="text" />
+      <div className="page-wrapper">
+        <div className="credit-card-visual">
+          <img src={MobileCreditCardBack} className='credit-card-back' alt="" />
+          <img src={MobileCreditCardFront} className='credit-card-front' alt="" />
+          <img src={MobileBackground} className='mobile-background' alt="" />
+          <img src={DesktopBackground} className='desktop-background' alt="" />
         </div>
-        <div className="credit-card-name">
-          <label>KART NUMARASI</label>
-          <input type="number" />
-        </div>
-        <div className="credit-card-expiration-date">
-          <div className="expiration-date-and-cvc">
-            <div className="expiration-date">
-              <label>SKT (AY/YIL)</label>
-              <div className="expiration-date-month-year">
-                <input type="number" />
+        <div className="credit-card-informations">
+          <div className="credit-card-name">
+            <label>KART SAHİBİNİN ADI</label>
+            <input type="text" />
+          </div>
+          <div className="credit-card-name">
+            <label>KART NUMARASI</label>
+            <input type="number" />
+          </div>
+          <div className="credit-card-expiration-date">
+            <div className="expiration-date-and-cvc">
+              <div className="expiration-date">
+                <label>SKT (AY/YIL)</label>
+                <div className="expiration-date-month-year">
+                  <input type="number" />
+                  <input type="number" />
+                </div>
+              </div>
+              <div className="credit-card-cvc">
+                <label>CVC</label>
                 <input type="number" />
               </div>
             </div>
-            <div className="credit-card-cvc">
-              <label>CVC</label>
-              <input type="number" />
-            </div>
           </div>
+          <button>Onayla</button>
         </div>
-        <button>Onayla</button>
       </div>
     </>
   )
